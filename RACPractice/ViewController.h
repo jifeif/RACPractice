1aa8fe59602e9cd852b227edddb740f28cba4c7f
吉飞飞 <jifeifei@9188.com>
2016-03-30 10:30:51 +0800

    Initial Commit

---
+++ RACPractice/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RACPractice
+//
+//  Created by 9188iMac on 16/3/30.
+//  Copyright © 2016年 9188iMac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ RACPractice/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  RACPractice
+//
+//  Created by 9188iMac on 16/3/30.
+//  Copyright © 2016年 9188iMac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
